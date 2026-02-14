4cf8f0bbf69ba90ea448b6b5777b83c33c06fc9f
kjerk <kjerk@pm.me>
2026-02-14 05:30:40 -0800

    Disable cli attach and help for now.

--- src/TimeToKill.App/Cli/CliOptions.cs
+++ src/TimeToKill.App/Cli/CliOptions.cs
@@ -9,24 +9,24 @@ public class CliOptions
 {
 	[Option("start-timer", Required = false, HelpText = "Start timer(s) by GUID or unique name. Repeatable.")]
 	public IEnumerable<string> StartTimers { get; set; }
-
+	
 	// Future:
 	// [Option("stop-timer", Required = false, HelpText = "Stop timer(s) by GUID or unique name. Repeatable.")]
 	// public IEnumerable<string> StopTimers { get; set; }
-
+	
 	// [Option("list", Required = false, Default = false, HelpText = "List all configured timer presets.")]
 	// public bool List { get; set; }
-
+	
 	[Option("help", Default = false, HelpText = "Display this help text.")]
 	public bool Help { get; set; }
-
+	
 	public bool HasCommands => StartTimers?.GetEnumerator().MoveNext() == true;
-
+	
 	private static Parser CreateParser() => new Parser(s => {
 		s.AutoHelp = false;
 		s.AutoVersion = false;
 	});
-
+	
 	public static CliOptions Parse(string[] args)
 	{
 		var parseResult = CreateParser().ParseArguments<CliOptions>(args);
@@ -34,7 +34,7 @@ public class CliOptions
 			return null;
 		return parseResult.Value;
 	}
-
+	
 	public static void PrintHelp()
 	{
 		var result = CreateParser().ParseArguments<CliOptions>(Array.Empty<string>());
--- src/TimeToKill.App/Program.cs
+++ src/TimeToKill.App/Program.cs
@@ -5,7 +5,6 @@ using Avalonia;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.FontAwesome;
 using TimeToKill.App.Cli;
-using TimeToKill.Tools;
 
 namespace TimeToKill.App;
 
@@ -13,49 +12,44 @@ public sealed class Program
 {
 	public static CliOptions StartupOptions { get; private set; }
 	public static SingleInstanceManager InstanceManager { get; private set; }
-
+	
 	[STAThread]
 	public static void Main(string[] args)
 	{
 		var options = CliOptions.Parse(args);
 		if (options == null)
 			return;
-
-		if (options.Help) {
-			NativeMethods.AttachToParentConsole();
-			CliOptions.PrintHelp();
-			return;
-		}
-
+		
 		InstanceManager = new SingleInstanceManager();
-
+		
 		if (!InstanceManager.TryAcquireInstance()) {
 			if (options.HasCommands) {
 				SendCommandsAndExit(options).GetAwaiter().GetResult();
 			}
+			
 			InstanceManager.Dispose();
 			InstanceManager = null;
 			return;
 		}
-
+		
 		StartupOptions = options;
 		BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-
+		
 		InstanceManager.Dispose();
 		InstanceManager = null;
 	}
-
+	
 	private static async Task SendCommandsAndExit(CliOptions options)
 	{
 		if (options.StartTimers?.Any() == true) {
 			await SingleInstanceManager.SendCommandToRunningInstance(IpcCommand.StartTimer(options.StartTimers));
 		}
 	}
-
+	
 	public static AppBuilder BuildAvaloniaApp()
 	{
 		IconProvider.Current.Register<FontAwesomeIconProvider>();
-
+		
 		return AppBuilder.Configure<App>()
 			.UsePlatformDetect()
 			.WithInterFont()
